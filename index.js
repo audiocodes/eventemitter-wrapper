4925cf5b4ec41cde71f343b83176e687df4fa391
Jason <unchosen.aus@gmail.com>
2021-08-15 00:51:58 +0800

    Use Map, WeakMap, Set & Improve once listeners

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 
 exports.createWrapper = (eventEmitterInstance)=>{
 	var wrappedEvents = Object.create(wrapperProto);
-	wrappedEvents._eventStore = {};
+	wrappedEvents._eventStore = new Map();
 	wrappedEvents._eventEmitterInstance = eventEmitterInstance;
 	wrappedEvents._eventRemoveListener = (eventName,listener)=>{
-		if(eventName in wrappedEvents._eventStore){
-			var pos = wrappedEvents._eventStore[eventName].indexOf(listener);
-			if(pos!==-1) wrappedEvents._eventStore[eventName].splice(pos,1);
-			if(wrappedEvents._eventStore[eventName].length===0) delete wrappedEvents._eventStore[eventName];
+		if(wrappedEvents._eventStore.has(eventName)){
+			if(wrappedEvents._eventStore.size===0) wrappedEvents._eventStore.delete(eventName);
 		}
 	};
 	wrappedEvents._eventRemoveListenerAttached = false;
+	wrappedEvents._eventOnceAlias = new WeakMap();
 	return wrappedEvents;
 };
 
 var wrapperProto = {
 	_eventRemoveListenerCheck: function(){
 		var hasEvents = false;
-		for(var e in this._eventStore){
-			if(this._eventStore[e].length>0){
-				hasEvents = true;
-				break;
-			}
+		for(let [eventName,set] of this._eventStore){
+			if(set.size>0){ hasEvents = true; break; }
 		}
 		if(this._eventRemoveListenerAttached && !hasEvents){
 			this._eventEmitterInstance.removeListener('removeListener',this._eventRemoveListener);
@@ -31,67 +27,73 @@ var wrapperProto = {
 			this._eventEmitterInstance.on('removeListener',this._eventRemoveListener);
 			this._eventRemoveListenerAttached = true;
 		}
+		for(let [eventName,set] of this._eventStore){
+			if(set.size===0) this._eventStore.delete(eventName);
+		}
 	},
 	addListener: function(a,b){
 		return this.on(a,b);
 	},
 	on: function(eventName,listener){
-		if(!(eventName in this._eventStore)) this._eventStore[eventName] = [];
-		this._eventStore[eventName].push(listener);
+		if(!this._eventStore.has(eventName)) this._eventStore.set(eventName,new Set());
+		this._eventStore.get(eventName).add(listener);
 		this._eventEmitterInstance.on(eventName,listener);
 		this._eventRemoveListenerCheck();
 		return this;
 	},
 	once: function(eventName,listener){
 		var listener2 = (...args)=>{
-			if(eventName in this._eventStore) this._eventStore[eventName].splice(this._eventStore[eventName].indexOf(listener2),1);
+			if(this._eventStore.has(eventName)) this._eventStore.get(eventName).delete(listener2);
 			return listener(...args);
 		};
-		if(!(eventName in this._eventStore)) this._eventStore[eventName] = [];
-		this._eventStore[eventName].push(listener2);
+		this._eventOnceAlias.set(listener,listener2);
+		if(!this._eventStore.has(eventName)) this._eventStore.set(eventName,new Set());
+		this._eventStore.get(eventName).add(listener2);
 		this._eventEmitterInstance.once(eventName,listener2);
 		this._eventRemoveListenerCheck();
 		return this;
 	},
 	listeners: function(eventName){
-		if(eventName in this._eventStore) return this._eventStore[eventName];
+		if(this._eventStore.has(eventName)) return this._eventStore.get(eventName).values();
 		else return [];
 	},
 	listenerCount: function(eventName){
 		return this.listeners(eventName).length;
 	},
 	eventNames: function(){
-		return Object.keys(this._eventStore);
+		return this._eventStore.keys();
 	},
 	emit: function(eventName,...args){
 		return this._eventEmitterInstance.emit(eventName,...args);
 	},
 	prependListener: function(eventName,listener){
-		if(!(eventName in this._eventStore)) this._eventStore[eventName] = [];
-		this._eventStore[eventName].unshift(listener);
+		if(!this._eventStore.has(eventName)) this._eventStore.set(eventName,new Set());
+		this._eventStore.get(eventName).add(listener);
 		this._eventEmitterInstance.prependListener(eventName,listener);
 		this._eventRemoveListenerCheck();
 		return this;
 	},
 	prependOnceListener: function(eventName,listener){
 		var listener2 = (...args)=>{
-			if(eventName in this._eventStore) this._eventStore[eventName].splice(this._eventStore[eventName].indexOf(listener2),1);
+			if(this._eventStore.has(eventName)) this._eventStore.get(eventName).delete(listener2);
 			return listener(...args);
 		};
-		if(!(eventName in this._eventStore)) this._eventStore[eventName] = [];
-		this._eventStore[eventName].unshift(listener2);
+		this._eventOnceAlias.set(listener,listener2);
+		if(!this._eventStore.has(eventName)) this._eventStore.set(eventName,new Set());
+		this._eventStore.get(eventName).add(listener2);
 		this._eventEmitterInstance.prependOnceListener(eventName,listener2);
 		this._eventRemoveListenerCheck();
 		return this;
 	},
 	removeAllListeners: function(eventName){
 		if(eventName===void 0){
-			for(var name in this._eventStore){
-				this.removeAllListeners(name);
+			for(let [eventName,set] of this._eventStore){
+				this.removeAllListeners(eventName);
 			}
-		} else if(eventName in this._eventStore){
-			for(var i=0,l=this._eventStore[eventName].length; i<l; i++){
-				this.removeListener(eventName,this._eventStore[eventName][i]);
+		} else if(this._eventStore.has(eventName)){
+			let set = this._eventStore.get(eventName);
+			for(let listener of set){
+				this.removeListener(eventName,listener);
 			}
 		}
 		this._eventRemoveListenerCheck();
@@ -99,8 +101,15 @@ var wrapperProto = {
 	},
 	removeListener: function(eventName,listener){
 		// Removal from this._eventStore is done on the 'removeListener' event
-		this._eventEmitterInstance.removeListener(eventName,listener);
+		if(listener){
+			this._eventEmitterInstance.removeListener(eventName,listener);
+			if(this._eventOnceAlias.has(listener)){
+				this._eventEmitterInstance.removeListener(eventName,this._eventOnceAlias.get(listener));
+				this._eventOnceAlias.delete(listener);
+			}
+		}
 		this._eventRemoveListenerCheck();
 		return this;
-	}
+	},
+	setMaxListeners: function(n){ return this._eventEmitterInstance.setMaxListeners(n); }
 };
